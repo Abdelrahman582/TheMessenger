48909c4ebdf6d7cd05c231a548ac95cc62d88ca2
Goldking582 <35976376+Goldking582@users.noreply.github.com>
2018-03-18 19:27:57 +0200

    Dbdumb

---
+++ dpdump.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+    <meta charset="utf-8">
+    <title>Database</title>
+    <link href="https://fonts.googleapis.com/css?family=Courgette" rel="stylesheet">
+	<link rel="icon" href="https://cdn4.iconfinder.com/data/icons/free-colorful-icons/360/messages.png">
+	<style>
+h1{
+	color:#3c1b1b;
+	text-shadow: 0 1px 0 #ccc,
+				 0 2px 0 #c9c9c9,
+				 0 3px 0 #bbb,
+				 0 4px 0 #b9b9b9,
+				 0 5px 0 #aaa,
+				 0 6px 1px rgba(0,0,0,.1),
+				 0 0 5px rgba(0,0,0,.1),
+				 0 1px 3px rgba(0,0,0,.3),
+				 0 3px 5px rgba(0,0,0,.2),
+				 0 5px 10px rgba(0,0,0,.25),
+				 0 10px 10px rgba(0,0,0,.2),
+				 0 20px 20px rgba(0,0,0,.15);
+}
+body{
+	margin: 150px;
+    padding: 0;
+	background-color: #e8f1f9;
+	background-size: auto, cover;
+	font-family: Courgette;
+}
+
+.container{ 
+  font-size: 30px;
+  margin:0 auto;
+  text-align: center;
+}
+.btn{
+	text-decoration: none;
+	background-color: rgb(244, 67, 54);
+    color: white;
+	width: 20%;
+    padding: 14px 25px;
+	margin: 15px 5px;
+    text-align: center;
+    display: inline-block;
+	cursor: pointer;
+	border-radius: 3px;
+}
+.btn:hover, .btn:focus , .btn:active {
+    background-color: #ff0000;
+}
+	</style>
+	</head>
+    
+    <body>
+        
+        <div class="container">
+            <h1>The Messenger </h1>
+            <a href="http://www.mediafire.com/file/2msnz3d47j2o7n2/Database" class="btn">Dump My Database</a>
+        </div>  
+    </body>
+</html>    
